bc97aa6e7d0ce16fccc33c7c81593654ada286d8
aphethean <aaron.phethean@gmail.com>
2015-11-21 09:21:10 +0000

    A new test that checks basic PDF rendering works as expected.  Related to issue observed on linux: java.lang.RuntimeException: Font 'Courier-BoldOblique' with 'Cp1252' is not recognized. 	at org.xhtmlrenderer.pdf.ITextFontResolver.createInitialFontMap(ITextFontResolver.java:470) 	at org.xhtmlrenderer.pdf.ITextFontResolver.<init>(ITextFontResolver.java:40) 	at org.xhtmlrenderer.pdf.ITextRenderer.<init>(ITextRenderer.java:120) 	at org.xhtmlrenderer.pdf.ITextRenderer.<init>(ITextRenderer.java:106) 	at org.xhtmlrenderer.pdf.ITextRenderer.<init>(ITextRenderer.java:102) 	at org.xhtmlrenderer.pdf.ITextRenderer.<init>(ITextRenderer.java:98)

---
+++ flying-saucer-pdf/src/test/java/org/xhtmlrenderer/pdf/TestITextRenderer.java
@@ -0,0 +1,39 @@
+package org.xhtmlrenderer.pdf;
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import org.junit.Test;
+
+import com.lowagie.text.DocumentException;
+
+public class TestITextRenderer {
+
+	@Test
+	public void testBasicRenderformXHTML() throws UnsupportedEncodingException,
+			IOException, URISyntaxException, DocumentException {
+		String yourXhtmlContentAsString = new String(Files.readAllBytes(Paths
+				.get(getClass().getResource(
+						"/TestITextRenderer-testBasicRenderformXHTML.xhtml")
+						.toURI())), "UTF-8");
+		String pdfOutputFilename = "target/TestITextRenderer-testBasicRenderformXHTML.pdf";
+		FileOutputStream fos = new FileOutputStream(pdfOutputFilename);
+
+		ITextRenderer renderer = new ITextRenderer();
+		renderer.setDocumentFromString(yourXhtmlContentAsString);
+		renderer.layout();
+		renderer.createPDF(fos);
+		fos.close();
+
+		assertTrue(new File(pdfOutputFilename).exists());
+	}
+
+}
